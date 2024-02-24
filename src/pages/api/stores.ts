c5816f97c2d57e828aecdf6b3fe9e3aa385c6629
junyub301 <junyub0301@gmail.com>
2024-02-24 22:06:42 +0900

    Feat: fetch to React-Query

--- src/pages/api/stores.ts
+++ src/pages/api/stores.ts
@@ -4,7 +4,9 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<StoreType[]>) {
     const prisma = new PrismaClient();
-    const stores = await prisma.store.findMany();
+    const stores = await prisma.store.findMany({
+        orderBy: { id: "asc" },
+    });
 
     res.status(200).json(stores);
 }
